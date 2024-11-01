f49c7a89a5641913e306e3be84725fd732226899
Bingshen <Cbs2315453671@gmail.com>
2024-11-01 14:55:44 +0100

    fronted init

---
+++ front-end/components/header.tsx
@@ -0,0 +1,38 @@
+import Link from 'next/link';
+
+const Header: React.FC = () => {
+  return (
+    <header className="p-3 mb-3 border-bottom bg-dark bg-gradient">
+
+      <a className="fs-2 d-flex justify-content-center mb-2 mb-lg-0 text-white-50 text-decoration-none">
+        {' '}
+        Simple WMS
+      </a>
+
+      <nav className="nav justify-content-center">
+        <Link href="/" className="nav-link px-4 fs-5 text-white">
+          Home
+        </Link>
+
+        <Link href="/inventories" className="nav-link px-4 fs-5 text-white">
+          Inventory
+        </Link>
+
+        <Link href="/orders" className="nav-link px-4 fs-5 text-white">
+          Order
+        </Link>
+
+        <Link href="/products" className="nav-link px-4 fs-5 text-white">
+          Product
+        </Link>
+
+        <Link href="/users" className="nav-link px-4 fs-5 text-white">
+          User Manangement
+        </Link>
+      </nav>
+
+    </header>
+  );
+};
+
+export default Header;
---
+++ front-end/types/index.ts
@@ -0,0 +1,53 @@
+export type User = {
+    userId: number;
+    username: string;
+    password: string;
+    role: string;
+    orders?: Order[];
+  };
+  
+  export type Product = {
+    productId: number;
+    name: string;
+    description: string;
+    location: string;
+    inventory?: Inventory;
+    orderDetails?: OrderDetail[];
+    orderOrderId?: number;
+    Order?: Order;
+  };
+  
+  export type Inventory = {
+    productId: number;
+    product: Product;
+    quantity: number;
+  };
+  
+  export type Order = {
+    orderId: number;
+    userId: number;
+    user: User;
+    products?: Product[];
+    status: string;
+    creationDate: Date;
+    orderDetails?: OrderDetail[];
+    shipments?: Shipment[];
+  };
+  
+  export type OrderDetail = {
+    orderDetailId: number;
+    orderId: number;
+    productId: number;
+    quantity: number;
+    order: Order;
+    product: Product;
+  };
+  
+  export type Shipment = {
+    shipmentId: number;
+    orderId: number;
+    status: string;
+    shippedDate: Date;
+    order: Order;
+  };
+  
