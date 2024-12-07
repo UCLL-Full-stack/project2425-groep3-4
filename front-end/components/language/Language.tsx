c4ac4c76035b96f6e769f85872060fb7abaf8bb3
Bingshen <Cbs2315453671@gmail.com>
2024-12-07 14:07:27 +0100

    Use language components, and translation

---
+++ front-end/components/language/Language.tsx
@@ -0,0 +1,83 @@
+import Select from "react-select";
+import { useRouter } from "next/router";
+
+const Language: React.FC = () => {
+  const router = useRouter();
+  const { locale } = router; 
+
+  const options = [
+    {
+      value: "nl",
+      label: (
+        <span>
+          <img
+            src="/images/nl.svg"
+            alt="Nederlands"
+            style={{ width: "16px", marginRight: "8px" }}
+          />
+          NL
+        </span>
+      ),
+    },
+    {
+      value: "en",
+      label: (
+        <span>
+          <img
+            src="/images/en.svg"
+            alt="English"
+            style={{ width: "16px", marginRight: "8px" }}
+          />
+          EN
+        </span>
+      ),
+    },
+    {
+      value: "es",
+      label: (
+        <span>
+          <img
+            src="/images/es.svg"
+            alt="Español"
+            style={{ width: "16px", marginRight: "8px" }}
+          />
+          ES
+        </span>
+      ),
+    },
+  ];
+
+  const handleLanguageChange = (selectedOption: any) => {
+    if (selectedOption && selectedOption.value) {
+      router.push(
+        {
+          pathname: router.pathname,
+          query: router.query,
+        },
+        router.asPath,
+        { locale: selectedOption.value }
+      ).then(() => {
+        router.reload(); 
+      });
+    }
+    console.log('Current locale:', locale);
+    console.log('Default value:', options.find((option) => option.value === locale));
+  };
+  
+  return (
+    <div className="d-flex align-items-center">
+      <label htmlFor="language" className="text-white me-2 fs-5">
+        Language:
+      </label>
+      <Select
+        id="language"
+        options={options}
+        defaultValue={options.find((option) => option.value === locale)} 
+        onChange={handleLanguageChange}
+        className="fs-5 bg-dark border-0"
+      />
+    </div>
+  );
+};
+
+export default Language;
--- front-end/pages/_app.tsx
+++ front-end/pages/_app.tsx
@@ -1,7 +1,13 @@
 import "@styles/globals.css";
 import type { AppProps } from "next/app";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { appWithTranslation } from "next-i18next";
 
-export default function App({ Component, pageProps }: AppProps) {
+console.log("next-i18next config loaded");
+
+const App = ({ Component, pageProps}: AppProps) => {
     return <Component {...pageProps} />;
 }
+console.log("next-i18next config loaded");
+
+export default appWithTranslation(App);
